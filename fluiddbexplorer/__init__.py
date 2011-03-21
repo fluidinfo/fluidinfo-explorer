10cb9077b7349c593a9a5d05922778c95c0b2ddc
PA Parent <paparent@paparent.me>
2011-03-21 15:13:22 -0400

    Refactoring: Removed some code duplication in __init__.py

--- fluiddbexplorer/__init__.py
+++ fluiddbexplorer/__init__.py
@@ -36,6 +36,22 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)
 
 
+def render_index(instance="main", username=None,
+                 rootid='nstree-disabled', rootlabel='Fluidinfo', **kwargs):
+    session['instance'] = instance
+
+    if not username:
+        username = session.get('username', 'Anonymous')
+
+    return render_template("index.html",
+                           username=username,
+                           rootlabel=rootlabel,
+                           instance=instance,
+                           baseurl_instance=direct.get_instance_url(instance),
+                           rootid=rootid,
+                           **kwargs)
+
+
 @app.route('/favicon.ico/')
 def favicon():
     abort(404)
@@ -45,9 +61,9 @@ def favicon():
 def index():
     username = session.get('username', 'Anonymous')
     if username == 'Anonymous':
-        return redirect(url_for('splash', instance='fluiddb'))
+        return redirect(url_for('splash', instance='main'))
     else:
-        return redirect(url_for('main', instance='fluiddb', rootns=username))
+        return redirect(url_for('main', instance='main', rootns=username))
 
 
 @app.route('/<instance>/')
@@ -56,61 +72,29 @@ def splash(instance):
     if username != 'Anonymous':
         return redirect(url_for('main', instance=instance, rootns=username))
     else:
-        session['instance'] = instance
-        return render_template("index.html",
-                               username=username,
-                               rootlabel='Fluidinfo',
-                               instance=instance,
-                               baseurl_instance=direct.get_instance_url(instance),
-                               rootid='nstree-disabled')
+        return render_index(username=username, instance=instance)
 
 
 @app.route('/<instance>/objects')
 def openquery(instance):
-    session['instance'] = instance
     query = request.args.get('query', '')
     query = query.replace('"', '\\"')
 
-    return render_template("index.html",
-                           username=session.get('username', 'Anonymous'),
-                           rootlabel='Fluidinfo',
-                           instance=instance,
-                           rootid='nstree-disabled',
-                           autoopenquery=query)
+    return render_index(instance=instance, autoopenquery=query)
 
 
 @app.route('/<instance>/object/<objectid>')
 @app.route('/<instance>/objects/<objectid>')
 def openobjectid(instance, objectid):
-    session['instance'] = instance
-    return render_template("index.html",
-                           username=session.get('username', 'Anonymous'),
-                           rootlabel='Fluidinfo',
-                           instance=instance,
-                           baseurl_instance=direct.get_instance_url(instance),
-                           rootid='nstree-disabled',
-                           autoopenobject=objectid)
+    return render_index(instance=instance, autoopenobject=objectid)
 
 
 @app.route('/<instance>/about/<about>')
 def openabout(instance, about):
-    session['instance'] = instance
-    return render_template("index.html",
-                           username=session.get('username', 'Anonymous'),
-                           rootlabel='Fluidinfo',
-                           instance=instance,
-                           baseurl_instance=direct.get_instance_url(instance),
-                           rootid='nstree-disabled',
-                           autoopenabout=about)
+    return render_index(instance=instance, autoopenabout=about)
 
 
 @app.route('/<instance>/<path:rootns>')
 def main(instance, rootns):
-    session['instance'] = instance
     rootns = rootns.rstrip('/')
-    return render_template("index.html",
-                           username=session.get('username', 'Anonymous'),
-                           rootlabel=rootns,
-                           instance=instance,
-                           baseurl_instance=direct.get_instance_url(instance),
-                           rootid=(rootns or 'nstree-disabled'))
+    return render_index(instance=instance, rootlabel=rootns, rootid=rootns)
